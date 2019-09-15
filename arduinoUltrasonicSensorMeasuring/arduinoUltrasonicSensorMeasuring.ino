485cb6eac2bd2572b492cc8f9102ea556670eb4a
Robert Nycz <robert.wiktor.nycz@gmail.com>
2019-09-15 21:42:12 +0100

    Ultrasonic Measuring Sensor and Arduino
    Code for using ultrasonic sensor with arduino, and getting results in serial monitor

--- arduinoUltrasonicSensorMeasuring/arduinoUltrasonicSensorMeasuring.ino
+++ arduinoUltrasonicSensorMeasuring/arduinoUltrasonicSensorMeasuring.ino
@@ -1,9 +1,48 @@
+// program will be measuring the distance using ultrasonic sensor HC-sr04
+// following the tutorial based on https://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
+// All math / phisics calculations are available under that link and in the code explaining
+// calculations, and in Readme
+// Sensor connection available under link: https://howtomechatronics.com/wp-content/uploads/2015/07/Ultrasonic-Sensor-Cirucit-Schematics-04.png
+// Enjoy :)
+
+// defining variables for Triger and Echo pins in module
+
+const int trigPin = 9; // trigger pin
+const int echoPin = 10; // echo pin
+
+// defining variables to calculate distance and period of travel time
+
+long duration;
+int distance;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode (trigPin, OUTPUT); // setting the trigPin as an Output
+  pinMode (echoPin, INPUT); // setting echoPin as an Input
+  Serial.begin(9600); // Start serial communication
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite (trigPin, LOW); // Clear the trigPin
+  delayMicroseconds(2); // Delay by 2 microseconds
+  // Set the trigPin on HIGH state for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
+  // Reads the echoPin, returns the travel time of sound wave in microseconds
+
+  duration = pulseIn(echoPin, HIGH);
+
+  // calculate the distance
+  // Distance is calculated by calculating the speed of sound (340m/s), in this case 0.034cm/microseconds
+  // and dividing the result by half because of the return wave
+  // and conversion of equation of speed (speed = distance / time )
+  // so distance = speed * time
+  
+  distance = duration * 0.034 / 2;
+
+  // print the distance in serial monitor
+  Serial.print(" Distance: ");
+  Serial.println(distance);
 }
